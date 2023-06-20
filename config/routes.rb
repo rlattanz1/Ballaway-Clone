bda4203545fe985c5a356bb80d18c40247b2251f
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-06-20 11:56:35 -0700

    ready for render deployment

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '*path', to: "static_pages#frontend_index"
 
 
   namespace :api, defaults: {format: :json} do
-      resources :users, only: [:create]
-      resource :session, only: [:create, :show, :destroy]
+    resources :users, only: [:create]
+    resource :session, only: [:create, :show, :destroy]
 
-  #   resources :reviews, only: [:index, :show, :create, :update, :destroy]
-  #   resources :items, only: [:index, :show, :create, :update, :destroy]
-  #   resources :users, only: [:create, :update, :destroy]
-  #   resources :cart_items, only: []
-  #   resource :session, only: [:create, :show, :destroy]
+    #   resources :reviews, only: [:index, :show, :create, :update, :destroy]
+    #   resources :items, only: [:index, :show, :create, :update, :destroy]
+    #   resources :users, only: [:create, :update, :destroy]
+    #   resources :cart_items, only: []
+    #   resource :session, only: [:create, :show, :destroy]
   end
+  get '*path', to: "static_pages#frontend_index"
 end
+
