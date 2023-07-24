878da0815398be5c8fe99bfdf4517939983ef8f1
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-07-24 11:37:29 -0700

    commiting before making a searchbar branch

--- frontend/src/components/Navigation/categories/CategorySlots/CategorySlot1.jsx
+++ frontend/src/components/Navigation/categories/CategorySlots/CategorySlot1.jsx
@@ -1,5 +1,21 @@
 import './CategorySlot1.css'
 
 export default function CategorySlot1() {
-    
+
+    // return (
+    //     <>
+    //         <h2>Gear Categories</h2>
+    //         <div className='gear-slot-container'>
+    //             <div className='balls-category-slot'>
+
+    //             </div>
+    //             <div className='bags-category-slot'>
+
+    //             </div>
+    //             <div className='gloves-category-slot'>
+
+    //             </div>
+    //         </div>
+    //     </>
+    // )
 }
--- frontend/src/components/Navigation/categories/CategorySlots/CategorySlot2.jsx
+++ frontend/src/components/Navigation/categories/CategorySlots/CategorySlot2.jsx
@@ -1,5 +1,29 @@
 import './CategorySlot2.css'
 
 export default function CategorySlot2() {
-    
+
+
+
+    // return (
+    //     <>
+    //         <h2>Club Categories</h2>
+    //         <div className='gear-slot-container'>
+    //             <div className='drivers-category-slot'>
+
+    //             </div>
+    //             <div className='woods-category-slot'>
+
+    //             </div>
+    //             <div className='irons-category-slot'>
+
+    //             </div>
+    //             <div className='wedges-category-slot'>
+
+    //             </div>
+    //             <div className='putters-category-slot'>
+
+    //             </div>
+    //         </div>
+    //     </>
+    // )
 }
