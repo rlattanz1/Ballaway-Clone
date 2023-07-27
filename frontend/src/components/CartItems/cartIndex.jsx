ece6f48ff408bcd244d90086eb18b1d98f7bf894
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-07-27 15:19:26 -0700

    got my prices but they need styling

--- frontend/src/components/CartItems/cartIndex.jsx
+++ frontend/src/components/CartItems/cartIndex.jsx
@@ -2,9 +2,8 @@ import CartIndexItem from "./cartIndexItem";
 import { fetchCartItems, getCartItems } from "../../store/cartItems";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts } from "../../store/products";
-import LoginFormModal from "../LoginFormModal";
 import './cartIndex.css'
+import { fetchProducts, getProducts } from "../../store/products";
 
 
 
@@ -12,48 +11,53 @@ import './cartIndex.css'
 
 export default function CartIndex() {
     const dispatch = useDispatch();
-    const userId = useSelector(state => state.session.user);
-    const [error, setErr] = useState('');
     const carts = useSelector(getCartItems);
+    const products = useSelector(getProducts);
 
     useEffect(() => {
         dispatch(fetchCartItems())
-        setErr('must be logged in')
+        dispatch(fetchProducts())
     }, [dispatch])
 
-    if (userId) {
-        return carts ? (
+    let totalSum = 0;
+    let totalTax = 0;
+return carts ? (
+        <div className="cart">
+            <div className="cart-right">
+                <div>
+                <h1 className="cart-title">Cart</h1>
+                    {carts.map(cartItem => (
+                        <CartIndexItem  cartItem={cartItem}/>
+                        ))}
+                </div>
+            </div>
+            <div className="cart-left">
+                <h1 className="purchase-total-title">Purchase Total</h1>
+                <div className="purchase-container">
+                    <div className="cart-cost">
+                        items total price
+                        <br />
+                        {carts.map(cartItem => {
+                            const product = products.find(product => product.id === cartItem.productId);
+                            totalSum += product.price * cartItem.quantity
+                            return (
+                                `(${product.price * cartItem.quantity})`
+                            )
 
-            <div className="cart">
-                <div className="cart-right">
-                    <div>
-                    <h1 className="cart-title">Cart</h1>
-                        {carts.map(cartItem => (
-                            <CartIndexItem  cartItem={cartItem}/>
-                            ))}
-                    </div>
-                </div>
-                <div className="cart-left">
-                    <h1 className="purchase-total-title">Purchase Total</h1>
-                    <div className="purchase-container">
-                        <div className="cart-cost">
-                            items total price
-                            <br/>
+                        })};
+                        <br/>
+                        <div className="total">
                             items total tax
-                            <br/>
-                            total purchase sum
+
+                            {totalTax = totalSum * 0.01587}
                         </div>
-                        <button className="purchase-button">Fake Purchase Items</button>
+                        <br/>
+                        total purchase sum
+                        {totalSum += totalTax}
                     </div>
+                    <button className="purchase-button">Fake Purchase Items</button>
                 </div>
             </div>
-        ) : null
-    }
-    else {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+        </div>
+    ) : null
 }
